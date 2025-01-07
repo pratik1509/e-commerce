001221359a9f9449529a8c6ce69310566cc06364
pratik1509 <gajjarpratikn@gmail.com>
2025-01-07 13:59:20 +0530

    Initial Commit

---
+++ ToDoList/TodoDb.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) : base (options){}
+
+    public DbSet<TodoItem> Todos { get; set; }    
+}
